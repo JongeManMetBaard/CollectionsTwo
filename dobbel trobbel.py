37fc05bf841b0b10cf51fa9d1e8aa4410abd5d2b
ali <ali.gout.12@hotmail.com>
2022-03-24 09:09:00 +0100

    dobbelsteen

--- dobbel trobbel.py
+++ dobbel trobbel.py
@@ -6,16 +6,51 @@ negativeNumForBlue = -2
 choiceCounterRed = 0
 choiceCounterBlue = 0
 
+firstNumRed = 2
+lastNumRed = 10
+
+firstNumBlue = 1
+lastNumBlue = 9
+
+
+redBlue = 0
+
+repeat = True
+
+# def checker(lijst, choice, position):
+
+#     if lijst == red:
+#         if lijst[position] == "":
+#             index = position 
+#             if index == 9:
+#                 return True
+#             while index < len(lijst - 1):
+#                 index += 1
+#                 if lijst[index] != "":
+#                     if lijst[index] > choice:
+#                         return True
+#     else:
+#         if lijst[position] == "":
+#             index = position 
+#             while index < len(lijst - 1):
+#                 index -= 1
+#                 if lijst[index] != "":
+#                     if lijst[index] > choice:
+#                         return True
+
+
 redDice = ["1", "2", "3", "4", "5", "6"]
 blueDice = ["1", "2", "3", "4", "5", "6"]
 whiteDice = ["1", "1", "1", "2", "2", "3"]
 
 choicePosition = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
-red =  [{negativeNumForRed}, "", "", "", "", "", "", "", "", ""] 
-blue = ["", "", "", "", "", "", "", "", "", {negativeNumForBlue}]
+red =  [negativeNumForRed, "", "", "", "", "", "", "", "", ""] 
+blue = ["", "", "", "", "", "", "", "", "", negativeNumForBlue]
+white = []
 
-for x in range(4):
+
+while repeat:
 
     randomRed = random.randint(1,6)
     randomBlue = random.randint(1,6)
@@ -34,7 +69,7 @@ for x in range(4):
     print(red)
     print(blue)
     print("----------------------------------------------------------------------------")
-    print("Scores White: |     |     |     |     |     |")
+    print("Scores White: |   {b[0]}  |     |     |     |     |")
     print("----------------------------------------------------------------------------")
     print("")
     print("Rolling dice........")
@@ -67,30 +102,64 @@ for x in range(4):
     elif choice == "d":
         choice = outcome_d
 
-    if randomRed > randomBlue:
-        
-        print(red)        
-        position = int(input("This is the list of the red row, choose a position fr\n"))
-        red[position] = choice
-        if choice < choiceCounterRed:
-            print("error")
-    elif randomBlue > randomRed:
+    if randomRed > randomBlue or redBlue == "r": 
+        print(red)
+        position = int(input(f"This is the list of the red row, choose a position from {firstNumRed} upto {lastNumRed}\n"))
+        red[position - 1] = choice
+        lijst = red
+
+    elif randomBlue > randomRed or redBlue == "b":
         print(blue)
-        position = int(input("This is the list of the blue row, choose a position\n"))
-        blue[position] = choice
-        if choice < choiceCounterBlue:
-            print("error")
-    elif randomRed == randomBlue:
-        if redBlue == "r":
-            print(red)
-            position = int(input("This is the list of the red row, choose a position\n"))
-            red[position] = choice
-        elif redBlue == "b":
-            print(blue)
-            position = int(input("This is the list of the blue row, choose a position\n"))
-            blue[position] = randomBlue
-    choiceCounterRed = choice
-    choiceCounterBlue = choice
+        position = int(input(f"This is the list of the blue row, choose a position from {firstNumBlue} upto {lastNumBlue}\n"))
+        blue[position - 1] = choice
+        lijst = blue
+
+    if choice == "c" or "d":
+        white.append(randomWhite)
+        print(white)
+
+    # if len(white) == 5 or len(red) == 10 and len(blue) == 10:
+    #     repeat = False
+
+    def checker(lijst,choice):
+        listOfNums = []
+        index = 0
+        
+        for x in lijst:
+            if lijst[index] != "":
+                index += 1
+            else:
+                leftValid = True
+                while leftValid:
+                    step = 1
+                    lijst[index - step]
+                    if lijst[index - step] != "":
+                        if lijst[index - step] < int(choice):
+                            step += 1
+                            listOfNums.append(index) 
+                            leftValid = False
+        rightValid = True
+        step = 1
+        index = 0
+        while rightValid:
+            for x in lijst:
+                if lijst[index] != "":
+                    index += 1
+
+                else:
+                    if lijst[index] == "":
+                        index += 1 
+                    if lijst[index] != "":
+                        if int(choice) > lijst[index]:
+                            step += 1
+                            listOfNums.append(index)
+                            rightValid = False
+                    if index == 9:
+                        rightValid = False
+                        break 
+        return listOfNums
+    print(checker(lijst,choice))                                           
+                    
 
 
     # def checkingPosition(red,blue):
