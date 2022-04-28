b4d04bc366e2b7989944d8c9fdaa270b6777d814
ali <ali.gout.12@hotmail.com>
2022-04-28 10:06:23 +0200

    spel eindigt als je geen getallen kan kwijtraken

--- dobbel trobbel2.py
+++ dobbel trobbel2.py
@@ -1,14 +1,99 @@
 import random 
 
-negativeNumForRed = -2
-negativeNumForBlue = -2
-
-listOfNums = "all positions except -2" 
 redBlue = ""
 
-red =  [negativeNumForRed, 0, 2, 4, 5, "", 7, 10, 11, ""] 
-blue = [14, 12, 11, "", 6, 5, "", 3, -1, negativeNumForBlue]
-white = [1,3,2,2,1]
+countRed = 1
+countBlue = 1
+countWhite = 0
+
+red =  [-2, 1, 2, 3, 4, "", 6, 7, 8, 9] 
+blue = [14, 12, 11, 7, 6, 5, 4, 3, -1, -2]
+white = [1,2,3,4,5]
+
+
+def checker(lijst,choice,pos):
+    index = pos
+    rightValid = True
+    global listOfNums
+    listOfNums = []
+
+    while rightValid:
+        if index == 10:
+            rightValid = False
+            break
+        if lijst[index] == int(choice):
+            index += 1 
+        elif lijst[index] == "":
+            index += 1
+        elif int(choice) < lijst[index] and lijst == red:
+            index += 1
+        elif int(choice) > lijst[index] and lijst == red:
+            print("Choose a different position")
+            redPos()
+        elif int(choice) < lijst[index] and lijst == blue:
+            print("Choose a different position")
+            bluePos()
+        elif int(choice) > lijst[index] and lijst == blue:
+            index += 1
+
+    index = pos
+    leftValid = True
+
+    while leftValid:
+        if index == -1:
+            leftValid = False
+            break
+        if lijst[index] == int(choice):
+            index -= 1
+        elif lijst[index] == "":
+            index -= 1
+        elif int(choice) > lijst[index] and lijst == red:
+            index -= 1
+        elif int(choice) < lijst[index] and lijst == red:
+            print("Choose a different position")
+            redPos()
+        elif int(choice) > lijst[index] and lijst == blue:
+            print("Choose a different position")
+            bluePos()
+        elif int(choice) < lijst[index] and lijst == blue:
+            index -= 1
+
+
+def redPos():   
+    position = int(input(f"Choose a position in the red row\n "))
+    if int(choice) > red[index] or int(choice) < red[index]:
+        repeat = False
+        endScore()
+    else:
+        lijst = red
+        checker(lijst,choice,position - 1)
+        red[position - 1] = choice
+        countRed += 1
+
+def bluePos():
+    position = int(input(f"Choose a position in the blue row\n"))
+    lijst = blue
+    checker(lijst,choice,position - 1)
+    blue[position - 1] = choice
+    global countBlue
+    countBlue += 1
+
+def endScore():
+    countRed = 10
+    countBlue = 10
+    if countRed == 10 and countBlue == 10 or countWhite == 5:
+        lst(red) 
+        lst(blue)
+        lst(white) 
+
+        subtotal_1 = 0 
+        for i in range(len(red)):
+            subtotal_1 += red[i] * blue[i]
+        
+        subtotal_2 = emptyPos * sum(white)
+        finalScore = subtotal_1 - subtotal_2
+        print(f"Your final score: {finalScore}")
+
 
 emptyPos = 0
 def lst(lijst):
@@ -17,7 +102,7 @@ def lst(lijst):
             lijst[i] = 0
             global emptyPos
             emptyPos += 1
-            
+     
 repeat = True
 while repeat:
 
@@ -25,8 +110,7 @@ while repeat:
     randomBlue = random.randint(1,6)
     randomWhite = random.randint(1,3)
 
-    outcome_a = -2
-    # randomRed + randomBlue + randomWhite
+    outcome_a = randomRed + randomBlue + randomWhite
     outcome_b = randomRed + randomBlue - randomWhite
     outcome_c = randomRed + randomBlue
     outcome_d = randomRed - randomWhite 
@@ -36,10 +120,10 @@ while repeat:
     print("----------------------------------------------------------------------------")
     print("position :    |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  10  |")
     print("----------------------------------------------------------------------------")
-    print("Scores Red:", red)
-    print("Scores Blue:", blue)
+    print(f"Scores Red:   | {red[0]}  |  {red[1]}  |  {red[2]}  |  {red[3]}  |  {red[4]}  |  {red[5]}   |  {red[6]}  |  {red[7]} |  {red[8]} |{red[9]}      |")
+    print(f"Scores Blue:  | {blue[0]}  |  {blue[1]} | {blue[2]}  |  {blue[3]}   |  {blue[4]}  |  {blue[5]}  |  {blue[6]}   |  {blue[7]}  | {blue[8]}  |  {blue[9]}  |")
     print("----------------------------------------------------------------------------")
-    print(f"Scores White: |     |     |     |    |     |")
+    print(f"Scores White: | {white[0]}    |  {white[1]}  |  {white[2]}    | {white[3]}   |  {white[4]}   |")
     print("----------------------------------------------------------------------------")
     print("")
     print("Rolling dice........")
@@ -48,7 +132,7 @@ while repeat:
     print("")
     print("----------------------------------------------------------------------------")
     print("Calculated results")
-    print(f"a: {-2} = {randomRed} + {randomBlue} + {randomWhite}")
+    print(f"a: {outcome_a} = {randomRed} + {randomBlue} + {randomWhite}")
     print(f"b: {outcome_b} = {randomRed} + {randomBlue} - {randomWhite}")
     print(f"c: {outcome_c} = {randomRed} + {randomBlue}")
     print(f"d: {outcome_d} = {randomRed} - {randomWhite}" )
@@ -69,91 +153,28 @@ while repeat:
         else:
             color = blue
 
+
     wrongChoice = True
     while wrongChoice:
+        listOutcomes = [outcome_a, outcome_b, outcome_c,outcome_d]
+        letters = ["a", "b", "c", "d"]
+
         choice = input("Please choose which result to register (a, b, c, d)\n")
-        if choice == "a":
-            choice = outcome_a
-        elif choice == "b":
-            choice = outcome_b
-        elif choice == "c": 
-            choice = outcome_c
-        elif choice == "d":
-            choice = outcome_d
+        letterChoice = choice
+        for index in range(0,4): 
+            if choice == letters[index]:
+                choice = listOutcomes[index]
+
         if choice in color:
             print("Choose a different number")
         else:
             wrongChoice = False
 
-
-    if color == red: 
-        print(red) 
-        position = int(input(f"This is the list of the red row, the positions you can choose are {listOfNums}\n "))
-        red[position - 1] = choice
-        lijst = red
-
+    if color == red:
+        redPos()
     elif color == blue:
-        print(blue)
-        position = int(input(f"This is the list of the blue row, the positions you can choose are {listOfNums}\n"))
-        blue[position - 1] = choice
-        lijst = blue
+        bluePos()        
 
-    if choice == "c" or "d":
+    if letterChoice == "c" or letterChoice == "d":
         white.append(randomWhite)
-        print(white)
-
-    if len(white) == 5 or len(red) == 10 and len(blue) == 10:
-        lst(red)
-        lst(blue)
-        lst(white) 
-
-        subtotal_1 = (-2*blue[0]) + (red[1]*blue[1]) + (red[2]*blue[2]) + (red[3]*blue[3])+ (red[4]*blue[4]) + (red[5]*blue[5]) + (red[6]*blue[6]) + (red[7]*blue[7])+ (red[8]*blue[8]) + (red[9]*-2)
-    	
-        subtotal_2 = emptyPos * (white[0] + white[1] + white[2] + white[3] + white[4]) 
-        finalScore = subtotal_1 - subtotal_2
-        print(f"Your final score: {finalScore}")
-
-        repeat = False
-        break
-
-
-    def checker(lijst,choice,pos):
-        index = pos
-        rightValid = True
-        global listOfNums
-        listOfNums = []
-
-        while rightValid:
-            if index == 10:
-                rightValid = False
-                break
-            if lijst[index] == int(choice):
-                index += 1
-            elif lijst[index] == "":
-                listOfNums.append(index + 1)
-                index += 1
-            elif int(choice) > lijst[index]:
-                index += 1
-            elif int(choice) < lijst[index]:
-                index += 1
-
-        index = pos
-        leftValid = True
-
-        while leftValid:
-            if index == -1:
-                leftValid = False
-                break
-            if lijst[index] == int(choice):
-                index -= 1
-            elif lijst[index] == "":
-                listOfNums.append(index + 1)
-                index -= 1
-            elif int(choice) > lijst[index]:
-                index -= 1
-            elif int(choice) < lijst[index]:
-                index -= 1
-
-        return listOfNums
-
-    print(checker(lijst,choice,position - 1))
+        countWhite += 1
